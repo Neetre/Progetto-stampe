4a365fa1e9b68ed4931cd925afed0b4794aac272
Neetree <19746@studenti.marconiverona.edu.it>
2023-02-06 13:25:04 +0100

    Ok champ

--- prod.py
+++ prod.py
@@ -6,25 +6,36 @@ import csv
 
 value_table = ["" for i in range (41)]
         
-def tabella():
+def tabella(fhtml):
 	global value_table
+
 	with open('odi_stampeA3Docenti.csv', newline='') as csvfile:
 		reader = csv.DictReader(csvfile, delimiter = ";")
 		fhtml.write("        <tr>")
+		attivo = False
 		for row in reader:
 			docente = row['docenti']
-			if docente != "ADAMOLI PAOLO":
-				continue
+			#if docente != "BELLINI GIANNI":
+				#continue
+			if attivo == False:
+				docente_attivo = docente
+				attivo = True
+			
 			giorno = row['giorno']
 			ora = row['ora']
 			classe = row['classe']
 			aula = row['aula']
 			materia = row['materia']
+
 			if value_table[0] != docente:
 				value_table = ["" for i in range (41)]
 			li(docente,  giorno, ora, classe, aula, materia)
-		doc()
-		fhtml.write("        </tr>")
+
+			if docente != docente_attivo:
+				#print(value_table)
+				doc()
+				fhtml.write("        </tr>")
+				docente_attivo = docente
 
 
 def doc():
@@ -47,74 +58,81 @@ def li(docente, giorno, ora, classe, aula, materia):
     #print(value_table)
 
 
-fhtml = open("stampa.html","w")
-fhtml.write('''<!DOCTYPE html5>
-<html>
-	<style>
-		table, tr, td {border:1px solid black;}
-	</style>
-	<body>
-		<table border='1' width='400px' style="width:100%">
-			<tr>
-				<th colspan='8' align="center">Docente</th>
-				<th colspan='8' align="center">Luned&igrave;</th>
-				<th colspan='8' align="center">Marted&igrave;</th>
-				<th colspan='8' align="center">Mercold&igrave;</th>
-				<th colspan='8' align="center">Gioved&igrave;</th>
-				<th colspan='8' align="center">Venerd&igrave;</th>
-			</tr>
-			<tr>
-				<th colspan="8" align='center'></th>
-				
-				<th align='center'>1</th>
-				<th align='center'>2</th>
-				<th align='center'>3</th>
-				<th align='center'>4</th>
-				<th align='center'>5</th>
-				<th align='center'>6</th>
-				<th align='center'>7</th>
-				<th align='center'>8</th>
+def header(fhtml):
+	fhtml.write('''<!DOCTYPE html5>
+	<html>
+		<style>
+			table, tr, td {border:1px solid black;}
+		</style>
+		<body>
+			<table border='1' width='400px' style="width:100%">
+				<tr>
+					<th colspan='8' align="center">Docente</th>
+					<th colspan='8' align="center">Luned&igrave;</th>
+					<th colspan='8' align="center">Marted&igrave;</th>
+					<th colspan='8' align="center">Mercold&igrave;</th>
+					<th colspan='8' align="center">Gioved&igrave;</th>
+					<th colspan='8' align="center">Venerd&igrave;</th>
+				</tr>
+				<tr>
+					<th colspan="8" align='center'></th>
+					
+					<th align='center'>1</th>
+					<th align='center'>2</th>
+					<th align='center'>3</th>
+					<th align='center'>4</th>
+					<th align='center'>5</th>
+					<th align='center'>6</th>
+					<th align='center'>7</th>
+					<th align='center'>8</th>
 
-				<th align='center'>1</th>
-				<th align='center'>2</th>
-				<th align='center'>3</th>
-				<th align='center'>4</th>
-				<th align='center'>5</th>
-				<th align='center'>6</th>
-				<th align='center'>7</th>
-				<th align='center'>8</th>
+					<th align='center'>1</th>
+					<th align='center'>2</th>
+					<th align='center'>3</th>
+					<th align='center'>4</th>
+					<th align='center'>5</th>
+					<th align='center'>6</th>
+					<th align='center'>7</th>
+					<th align='center'>8</th>
 
-				<th align='center'>1</th>
-				<th align='center'>2</th>
-				<th align='center'>3</th>
-				<th align='center'>4</th>
-				<th align='center'>5</th>
-				<th align='center'>6</th>
-				<th align='center'>7</th>
-				<th align='center'>8</th>
+					<th align='center'>1</th>
+					<th align='center'>2</th>
+					<th align='center'>3</th>
+					<th align='center'>4</th>
+					<th align='center'>5</th>
+					<th align='center'>6</th>
+					<th align='center'>7</th>
+					<th align='center'>8</th>
 
-				<th align='center'>1</th>
-				<th align='center'>2</th>
-				<th align='center'>3</th>
-				<th align='center'>4</th>
-				<th align='center'>5</th>
-				<th align='center'>6</th>
-				<th align='center'>7</th>
-				<th align='center'>8</th>
-				
-				<th align='center'>1</th>
-				<th align='center'>2</th>
-				<th align='center'>3</th>
-				<th align='center'>4</th>
-				<th align='center'>5</th>
-				<th align='center'>6</th>
-				<th align='center'>7</th>
-				<th align='center'>8</th>
-			</tr>''')
-tabella()
-fhtml.write("    </table>")
-fhtml.write("</body>")
-fhtml.write("</html>")
-fhtml.close()
+					<th align='center'>1</th>
+					<th align='center'>2</th>
+					<th align='center'>3</th>
+					<th align='center'>4</th>
+					<th align='center'>5</th>
+					<th align='center'>6</th>
+					<th align='center'>7</th>
+					<th align='center'>8</th>
+					
+					<th align='center'>1</th>
+					<th align='center'>2</th>
+					<th align='center'>3</th>
+					<th align='center'>4</th>
+					<th align='center'>5</th>
+					<th align='center'>6</th>
+					<th align='center'>7</th>
+					<th align='center'>8</th>
+				</tr>''')
+ 
+def footer(fhtml):
+	fhtml.write("    </table>")
+	fhtml.write("</body>")
+	fhtml.write("</html>")
+	fhtml.close()
+
+if __name__ == "__main__":
+    fhtml = open("stampa.html","w")
+    header(fhtml)
+    tabella(fhtml)
+    footer(fhtml)
 
 print(value_table)
